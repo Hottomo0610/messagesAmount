c6f5ef4058fae5a315d1cf263440e2a1eb508cad
Hottomo0610 <56496269+Hottomo0610@users.noreply.github.com>
2021-01-18 00:18:41 +0900

    Update createTextAndSendSlack.php

--- createTextAndSendSlack.php
+++ createTextAndSendSlack.php
@@ -78,7 +78,7 @@ class createTextAndSendSlack {
 
     public function send_to_slack($rawJson){
         //CURLでSlackに送信
-        $ch = curl_init("https://hooks.slack.com/services/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");
+        $ch = curl_init("https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXX");
         curl_setopt( $ch, CURLOPT_POSTFIELDS, $rawJson );
         curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
